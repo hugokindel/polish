8cfd8abde0310363d0c5c5cf9d5d099d7b3e0f31
KINDEL Hugo <hugokindel.pro@protonmail.com>
2021-11-24 11:54:30 +0100

    Add documentation

--- polish.ml
+++ polish.ml
@@ -54,6 +54,7 @@ type line = position * indentation * string list
 
 (** Returns the indentation of line (a list of words). *)
 let get_indentation (words: string list): int =
+
   let rec get_indentation_rec words acc =
     match words with
     | [] -> acc / 2
@@ -61,6 +62,7 @@ let get_indentation (words: string list): int =
       match word with
       | "" -> get_indentation_rec words' (acc + 1)
       | _ -> acc / 2 in
+
   get_indentation_rec words 0
 
 (** Returns a line (a list of words) without empty words. *)
@@ -76,22 +78,31 @@ let clean_words (words: string list): string list =
 
 (** Reads a Polish file and returns a list of lines (without comment lines and without empty words). *)
 let read_file (filename: string): line list =
+
   let rec read_file_rec (ic: in_channel) (lines_acc: line list) (position_acc: int) =
     try
+      (* Split a line into words. *)
       let words = String.split_on_char ' ' (input_line ic) in
       let indent = get_indentation words in
       let words = clean_words words in
       let pos = position_acc + 1 in
-      if String.equal (List.hd words) "COMMENT" then
+      if List.length words = 0 then
+        (* If a line is empty, ignore it. *)
         read_file_rec ic lines_acc pos
       else
-        let line = ((pos, indent, words): line) in
-        read_file_rec ic (lines_acc @ [line]) pos
+        if String.equal (List.hd words) "COMMENT" then
+          (* If a line start with `COMMENT`, ignore it. *)
+          read_file_rec ic lines_acc pos
+        else
+          (* Else, add it to the list of lines. *)
+          let line = ((pos, indent, words): line) in
+          read_file_rec ic (lines_acc @ [line]) pos
     with e ->
       if e = End_of_file then
         (close_in ic; lines_acc)
       else
         (close_in_noerr ic; failwith "polish: cannot read file") in
+
   let ic = open_in filename in
   read_file_rec ic [] 0
 
@@ -100,22 +111,30 @@ let rec parse_expr (words: string list): expr * string list =
   match words with
   | [] -> failwith "polish: cannot parse expression: `words` is empty"
   | word::words' ->
+
+    (* Check if a given word is an integer (by comparing it with a regex). *)
     let is_integer word =
       Str.string_match (Str.regexp "^[+-]?[0-9]+$") word 0 in
+
+    (* Check if a given word is an arithmetic operator. *)
     let is_op word =
       String.equal word "+" || String.equal word "-" || String.equal word "*" ||
       String.equal word "/" || String.equal word "%" in
+
     if is_integer word then
       ((Num (int_of_string word)), words')
     else if (is_op word) then
-      let (expr1, nextWords) = parse_expr words' in
-      let (expr2, nextWords') = parse_expr nextWords in
+      (* Parse the first expression of the arithmetic expression. *)
+      let (expr1, next_words) = parse_expr words' in
+      (* Parse the second expression of the arithmetic expression. *)
+      let (expr2, next_words') = parse_expr next_words in
+      (* Parse the  arithmetic operator. *)
       match word with
-      | "+" -> (Op (Add, expr1, expr2)), nextWords'
-      | "-" -> (Op (Sub, expr1, expr2)), nextWords'
-      | "*" -> (Op (Mul, expr1, expr2)), nextWords'
-      | "/" -> (Op (Div, expr1, expr2)), nextWords'
-      | "%" -> (Op (Mod, expr1, expr2)), nextWords'
+      | "+" -> (Op (Add, expr1, expr2)), next_words'
+      | "-" -> (Op (Sub, expr1, expr2)), next_words'
+      | "*" -> (Op (Mul, expr1, expr2)), next_words'
+      | "/" -> (Op (Div, expr1, expr2)), next_words'
+      | "%" -> (Op (Mod, expr1, expr2)), next_words'
       | _ -> failwith "polish: cannot parse `%` expression: unknown arithmetic operator"
     else
       ((Var (word: name)), words')
@@ -125,23 +144,28 @@ let parse_cond (words: string list): cond * string list =
   match words with
   | [] -> failwith "polish: cannot parse condition: `words` is empty"
   | _ ->
-    let (left_expr, nextWords) = parse_expr words in
-    match nextWords with
-    | [] -> failwith "polish: cannot parse condition: `nextWords` is empty"
+    (* Parse the expression at the left of a condition. *)
+    let (left_expr, next_words) = parse_expr words in
+    match next_words with
+    | [] -> failwith "polish: cannot parse condition: `next_words` is empty"
     | word::words' ->
-      let (right_expr, nextWords') = parse_expr words' in
+      (* Parse the expression at the right of a condition. *)
+      let (right_expr, next_words') = parse_expr words' in
+      (* Parse the comparison operator. *)
       match word with
-      | "=" -> (left_expr, Eq, right_expr), nextWords'
-      | "<>" -> (left_expr, Ne, right_expr), nextWords'
-      | "<"-> (left_expr, Lt, right_expr), nextWords'
-      | "<=" -> (left_expr, Le, right_expr), nextWords'
-      | ">" -> (left_expr, Gt, right_expr), nextWords'
-      | ">=" -> (left_expr, Ge, right_expr), nextWords'
+      | "=" -> (left_expr, Eq, right_expr), next_words'
+      | "<>" -> (left_expr, Ne, right_expr), next_words'
+      | "<"-> (left_expr, Lt, right_expr), next_words'
+      | "<=" -> (left_expr, Le, right_expr), next_words'
+      | ">" -> (left_expr, Gt, right_expr), next_words'
+      | ">=" -> (left_expr, Ge, right_expr), next_words'
       | _ -> failwith "polish: cannot parse condition: unknown comparison operator"
 
 (** Parses a code block. *)
 let parse_block (lines: line list): block =
+
   let rec parse_block_rec lines block_indent acc =
+
     let parse_set pos indent words lines =
       match words with
       | [] -> failwith "polish: cannot parse block: instruction missing"
@@ -150,10 +174,12 @@ let parse_block (lines: line list): block =
         | [] -> failwith "polish: cannot parse block: unknown instruction"
         | op::words' ->
           if String.equal op ":=" then
+            (* If the operator `:=` is found we are parsing a `SET`. *)
             let (expr, _) = parse_expr words' in
             let instr = Set (name, expr) in
             parse_block_rec lines indent (acc @ [pos, instr])
           else
+            (* We are parsing something unexpected. *)
             failwith "polish: cannot parse block: unknown instruction" in
 
     let parse_print pos indent words lines =
@@ -166,29 +192,32 @@ let parse_block (lines: line list): block =
       parse_block_rec lines indent (acc @ [pos, instr]) in
 
     let parse_if pos indent words lines =
-      let (if_block, nextWords) = parse_block_rec  lines (indent + 1) [] in
+      let (if_block, next_words) = parse_block_rec  lines (indent + 1) [] in
       let (cond, _) = parse_cond words in
-      (match nextWords with
+      (match next_words with
       | [] ->
+        (* If there is no instructions after the `IF`, parse it and continue parsing. *)
         let instr = If (cond, if_block, []) in
-        parse_block_rec nextWords indent (acc @ [pos, instr])
-      | nextLine::nextLines ->
-        match nextLine with
+        parse_block_rec next_words indent (acc @ [pos, instr])
+      | next_line::next_lines ->
+        match next_line with
         | (_, _, words') ->
           match List.hd words' with
           | "ELSE" ->
-            let (else_block, nextLines') = parse_block_rec nextLines (indent + 1) [] in
+            (* If there is an `ELSE` block, parse its block too and continue parsing. *)
+            let (else_block, next_lines') = parse_block_rec next_lines (indent + 1) [] in
             let instr = If (cond, if_block, else_block) in
-            parse_block_rec nextLines' indent (acc @ [pos, instr])
+            parse_block_rec next_lines' indent (acc @ [pos, instr])
           | _ ->
+            (* If there are others instructions after the `IF`, parse it and continue parsing. *)
             let instr = If (cond, if_block, []) in
-            parse_block_rec nextWords indent (acc @ [pos, instr])) in
+            parse_block_rec next_words indent (acc @ [pos, instr])) in
 
     let parse_while pos indent words lines =
-      let (while_block, nextWords) = parse_block_rec  lines (indent + 1) [] in
+      let (while_block, next_words) = parse_block_rec  lines (indent + 1) [] in
       let (cond, _) = parse_cond words in
       let instr = While (cond, while_block) in
-      parse_block_rec nextWords indent (acc @ [pos, instr]) in
+      parse_block_rec next_words indent (acc @ [pos, instr]) in
 
     match lines with
     | [] -> (acc, lines)
@@ -242,15 +271,18 @@ let string_of_cond (cond: cond): string =
 
 (** Prints a line indentation to stdout. *)
 let print_indent (indent: indentation): unit =
+
   let rec print_indent_rec (indent: indentation) (acc: string): unit =
     if indent > 0 then
       print_indent_rec (indent - 1) (acc ^ "  ")
     else
       print_string acc in
+
   print_indent_rec indent ""
 
 (** Prints a Polish block to stdout. *)
 let print_block (block: block): unit =
+
   let rec print_block_rec block indent: unit =
     match block with
     | [] -> ()
@@ -260,19 +292,21 @@ let print_block (block: block): unit =
       | Set (name, expr) -> printf "%s := %s\n" name (string_of_expr expr);
       | Print (expr) -> printf "PRINT %s\n" (string_of_expr expr);
       | Read (name) -> printf "READ %s\n" name;
-      | If (cond, block1, block2) ->
+      | If (cond, if_block, else_block) ->
         printf "IF %s\n" (string_of_cond cond);
-        print_block_rec block1 (indent + 1);
-        (match block2 with
+        print_block_rec if_block (indent + 1);
+        (match else_block with
         | [] -> ()
         | _ ->
+          (* If there is an `ELSE` block, print it too. *)
           print_indent indent;
           printf "ELSE\n";
-          print_block_rec block2 (indent + 1));
+          print_block_rec else_block (indent + 1));
       | While (cond, block) ->
         printf "WHILE %s\n" (string_of_cond cond);
         print_block_rec block (indent + 1));
       print_block_rec block' indent in
+
   print_block_rec block 0
 
 (** Reads a Polish program from a filename. *)
